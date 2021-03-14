8b2b9b7ea6b6972c270cf9d16e0b255e519abfd7
Monica Mateiu <monica.mateiu@gmail.com>
2021-03-14 20:57:51 +0000

    generate recipes controller and add index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/edit'
+  get 'recipes/update'
+  get 'recipes/destroy'
   root 'dashboard#index'
   get 'dashboard/index'
 end
