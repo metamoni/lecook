3d5517cb8d893efe32b4c65834ab2c68b4393ffc
Monica Mateiu <monica.mateiu@gmail.com>
2021-03-28 12:58:20 +0100

    add ingredient model and controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
+  has_and_belongs_to_many :ingredients
   has_many :menus
   has_many :meal_plans, through: :menus
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :ingredients, only: %i[new index create destroy]
   resources :recipes
   resources :meal_plans, only: %i[new index show create]
   root 'dashboard#index'
