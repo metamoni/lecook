8f4faf6ccf34b6174e1abef1a5c49d76647aae65
Monica Mateiu <monica.mateiu@gmail.com>
2021-04-05 11:15:37 +0100

    accept nested attributes for ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,10 @@ class Recipe < ApplicationRecord
   has_and_belongs_to_many :ingredients
   has_many :menus
   has_many :meal_plans, through: :menus
+  
+  has_rich_text :instructions
+
+  accepts_nested_attributes_for :ingredients
 
   validates :title, :instructions, presence: true
-  has_rich_text :instructions
 end
