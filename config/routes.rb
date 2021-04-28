681a3efcc2890e26114587a67bdc4ea1a260af39
Monica Mateiu <monica.mateiu@gmail.com>
2021-04-28 18:44:51 +0100

    update root and recipes index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :ingredients, only: %i[new index create destroy]
   resources :recipes
   resources :meal_plans
-  root 'dashboard#index'
+  root 'recipes#index'
   get 'dashboard/index'
 end
